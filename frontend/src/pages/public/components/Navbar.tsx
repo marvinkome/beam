d65f2fc9bcfe69ee779f239e812260eaa9602d18
Lenny Johnson <43718761+thelennyjohnson@users.noreply.github.com>
2020-08-31 17:05:42 +0100

    remove login button

--- frontend/src/pages/public/components/Navbar.tsx
+++ frontend/src/pages/public/components/Navbar.tsx
@@ -5,23 +5,13 @@ import { trackEvent } from "lib/analytics"
 import "./Navbar.scss"
 
 export function Navbar() {
-    const { signIn, loaded } = useGoogleLogin(true)
+  
 
     return (
         <nav className="public-navbar">
             <div>
                 <img alt="beam" src={require("assets/images/beam-logo-dark.png")}></img>
             </div>
-
-            <button
-                onClick={() => {
-                    signIn()
-                    trackEvent("Login with Google", { category: "Auth" })
-                }}
-                className={cls("btn", { disabled: !loaded })}
-            >
-                Log in
-            </button>
         </nav>
     )
 }
