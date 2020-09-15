138890b5449c6f4abcc82108a0ac8ae2fd06e44e
Marvin Kome <marvinkome@gmail.com>
2020-09-16 00:36:22 +0100

    update privacy policy

--- frontend/src/pages/public/privacy-policy/index.tsx
+++ frontend/src/pages/public/privacy-policy/index.tsx
@@ -541,6 +541,7 @@ export function PrivacyPolicy() {
                                 <strong>Functionality and Infrastructure Optimization</strong>
                             </p>
                             <p>MongoDB Atlas</p>
+                            <p>Firebase</p>
                         </li>
                         <li>
                             <p>
